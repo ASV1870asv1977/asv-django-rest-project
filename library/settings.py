0e58649851725a27dfb513b7729ffbdf00f23d17
r2d2 <aver.serg1870@gmail.com>
2021-08-24 14:22:10 +0300

    created app authors

--- library/settings.py
+++ library/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3d part apps
+    # custom apps
+    'authors',
 ]
 
 MIDDLEWARE = [
