c9ace4f9115ea6e56340bcadc2ff1e8fc3d1b15e
r2d2 <aver.serg1870@gmail.com>
2021-08-27 12:30:58 +0300

    lesson_2 created frontend React-app

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = [];
+    }
+
+    render() {
+        return (
+            <div>Main App</div>
+        )
+    }
+}
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h2>Hello world</h2>
+//     </div>
+//   );
+// }
+
+export default App;
