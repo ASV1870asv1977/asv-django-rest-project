1d6251b0b4cfb42600e27ea54cc59b5b38e33832
r2d2 <aver.serg1870@gmail.com>
2021-08-27 15:07:00 +0300

    lesson_2 part_2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import AuthorItem from "./components/Authors";
+import AuthorList from "./components/Authors";
 
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = [];
+        this.state = {
+            'authors': []
+        };
+    }
+
+    componentDidMount() {
+        const authors = [
+            {
+                'first_name': "Иван",
+                'last_name': "Иванов",
+                'birthday_year': 1990
+            },
+            {
+                'first_name': "Сергей",
+                'last_name': "Сергеев",
+                'birthday_year': 1970
+            }
+        ]
+        // this.state.authors = authors;
+        this.setState(
+            this.state = {
+                'authors': authors
+            }
+        )
     }
 
     render() {
         return (
-            <div>Main App</div>
+            // <div>Main App</div>
+            // <AuthorItem author={{
+            //     'first_name': "Иван",
+            //     'last_name': "Иванов",
+            //     'birthday_year': 1990
+            // }}/>
+
+            <AuthorList authors={this.state.authors}/>
         )
     }
 }
